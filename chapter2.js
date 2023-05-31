eb6dbfafe65b171713a8af99b5af64c92878a789
radhikadudam <134921297+radhikadudam@users.noreply.github.com>
2023-05-31 13:46:29 +0530

    Add files via upload

---
+++ chapter2.js
@@ -0,0 +1,5 @@
+let body = document.getElementsByClassName("gig")
+let btn = document.getElementsByClassName("btn")[0]
+btn.addEventListener("click", function () {
+    document.write("you clicked here")
+})
