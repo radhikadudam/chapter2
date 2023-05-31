78ff0d72b774e46f2b1bd73af37f0ef674106f73
radhikadudam <134921297+radhikadudam@users.noreply.github.com>
2023-05-31 14:23:10 +0530

    Delete chapter2.js

--- chapter2.js
+++
@@ -1,5 +0,0 @@
-let body = document.getElementsByClassName("gig")
-let btn = document.getElementsByClassName("btn")[0]
-btn.addEventListener("click", function () {
-    document.write("you clicked here")
-})
